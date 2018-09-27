8e10754d77aa63a0411ad41eb4785f0bcbaaf281
Ondrej Zizka <zizka@seznam.cz>
2018-09-27 02:36:20 +0200

    Reformat

--- src/main/java/cz/dynawest/logging/LoggingUtils.java
+++ src/main/java/cz/dynawest/logging/LoggingUtils.java
@@ -6,46 +6,52 @@ import java.io.InputStream;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-public class LoggingUtils {
-   private static final Logger log = Logger.getLogger(LoggingUtils.class.getName());
-   private static final String DEFAULT_CLASSPATH_SEARCH_PATH = "/logging.properties";
-   private static final String DW_DEFAULT_PROPS_PATH = "/cz/dynawest/logging/logging-default.properties";
+public class LoggingUtils
+{
+    private static final Logger log = Logger.getLogger(LoggingUtils.class.getName());
+    private static final String DEFAULT_CLASSPATH_SEARCH_PATH = "/logging.properties";
+    private static final String DW_DEFAULT_PROPS_PATH = "/cz/dynawest/logging/logging-default.properties";
 
-   public static void initLogging() {
-      initLogging("#/logging.properties");
-   }
+    public static void initLogging()
+    {
+        initLogging("#/logging.properties");
+    }
 
-   public static void initLogging(String filePath) {
-      boolean wasFromSysProp = true;
-      String logConfigFile = System.getProperty("java.util.logging.config.file");
-      if(logConfigFile == null) {
-         logConfigFile = filePath;
-         wasFromSysProp = false;
-      }
+    public static void initLogging(String filePath)
+    {
+        boolean wasFromSysProp = true;
+        String logConfigFile = System.getProperty("java.util.logging.config.file");
+        if (logConfigFile == null) {
+            logConfigFile = filePath;
+            wasFromSysProp = false;
+        }
 
-      try {
-         Object ex;
-         if(logConfigFile.startsWith("#")) {
-            ex = LoggingUtils.class.getResourceAsStream(logConfigFile.substring(1));
-         } else {
-            try {
-               ex = new FileInputStream(logConfigFile);
-            } catch (IOException var5) {
-               ex = null;
+        try {
+            Object ex;
+            if (logConfigFile.startsWith("#")) {
+                ex = LoggingUtils.class.getResourceAsStream(logConfigFile.substring(1));
             }
-         }
+            else {
+                try {
+                    ex = new FileInputStream(logConfigFile);
+                }
+                catch (IOException var5) {
+                    ex = null;
+                }
+            }
 
-         log.info("Loading logging conf from: " + logConfigFile + (!wasFromSysProp?"":" (set in sys var java.util.logging.config.file)"));
-         if(ex == null) {
-            log.warning("Log config file not found: " + logConfigFile + "  Using LoggingUtils\' default.");
-            logConfigFile = "/cz/dynawest/logging/logging-default.properties";
-            ex = LoggingUtils.class.getResourceAsStream("/cz/dynawest/logging/logging-default.properties");
-         }
+            log.info("Loading logging conf from: " + logConfigFile + (!wasFromSysProp ? "" : " (set in sys var java.util.logging.config.file)"));
+            if (ex == null) {
+                log.warning("Log config file not found: " + logConfigFile + "  Using LoggingUtils\' default.");
+                logConfigFile = "/cz/dynawest/logging/logging-default.properties";
+                ex = LoggingUtils.class.getResourceAsStream("/cz/dynawest/logging/logging-default.properties");
+            }
 
-         LogManager.getLogManager().readConfiguration((InputStream)ex);
-      } catch (IOException var6) {
-         System.err.println("Error loading logging conf from [" + logConfigFile + "]. Using JDK\'s default.");
-      }
+            LogManager.getLogManager().readConfiguration((InputStream) ex);
+        }
+        catch (IOException var6) {
+            System.err.println("Error loading logging conf from [" + logConfigFile + "]. Using JDK\'s default.");
+        }
 
-   }
+    }
 }
