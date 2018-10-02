97e925dec59bde63120bfbf0e1f970e80ad1dc15
Ondrej Zizka <zizka@seznam.cz>
2018-10-02 20:58:55 +0200

    Fixed test params in POM

--- src/main/java/cz/dynawest/csvcruncher/App.java
+++ src/main/java/cz/dynawest/csvcruncher/App.java
@@ -36,21 +36,22 @@ public class App
         App.OptionsNext next = null;
 
         for (int i = 0; i < args.length; ++i) {
-            String str = args[i];
+            String arg = args[i];
+            System.out.println("XXX " + arg);
 
             // JSON output
-            if (str.startsWith("--json")) {
-                if (str.endsWith("=" + Options.JsonExportFormat.ARRAY.getOptionValue()))
+            if (arg.startsWith("--json")) {
+                if (arg.endsWith("=" + Options.JsonExportFormat.ARRAY.getOptionValue()))
                     opt.jsonExportFormat = Options.JsonExportFormat.ARRAY;
                 else
                     opt.jsonExportFormat = Options.JsonExportFormat.ENTRY_PER_LINE;
             }
 
             // Row numbers
-            else if (str.startsWith("--rowNumbers")) {
+            else if (arg.startsWith("--rowNumbers")) {
                 opt.initialRowNumber = -1L;
-                if (str.startsWith("--rowNumbers=")) {
-                    String numberStr = StringUtils.removeStart(str, "--rowNumbers=");
+                if (arg.startsWith("--rowNumbers=")) {
+                    String numberStr = StringUtils.removeStart(arg, "--rowNumbers=");
                     try {
                         long number = Long.parseLong(numberStr);
                         opt.initialRowNumber = number;
@@ -61,65 +62,70 @@ public class App
             }
 
             // Combine input files
-            else if (str.startsWith("--" + Options.CombineInputFiles.OPTION)) {
-                if (str.endsWith("=" + Options.CombineInputFiles.INTERSECT.getOptionValue()))
+            else if (arg.startsWith("--" + Options.CombineInputFiles.OPTION)) {
+                if (arg.endsWith("=" + Options.CombineInputFiles.INTERSECT.getOptionValue()))
                     opt.combineInputFiles = Options.CombineInputFiles.INTERSECT;
-                else if (str.endsWith("=" + Options.CombineInputFiles.EXCEPT.getOptionValue()))
+                else if (arg.endsWith("=" + Options.CombineInputFiles.EXCEPT.getOptionValue()))
                     opt.combineInputFiles = Options.CombineInputFiles.EXCEPT;
                 else
                     opt.combineInputFiles = Options.CombineInputFiles.CONCAT;
             }
 
             // Combine files in input directories
-            else if (str.startsWith("--" + Options.CombineDirectories.OPTION)) {
-                if (str.endsWith("=" + Options.CombineDirectories.COMBINE_PER_EACH_DIR.getCombineMode()))
+            else if (arg.startsWith("--" + Options.CombineDirectories.OPTION)) {
+                if (arg.endsWith("=" + Options.CombineDirectories.COMBINE_PER_EACH_DIR.getCombineMode()))
                     opt.combineDirs = Options.CombineDirectories.COMBINE_PER_EACH_DIR;
-                else if (str.endsWith("=" + Options.CombineDirectories.COMBINE_PER_INPUT_SUBDIR.getCombineMode()))
+                else if (arg.endsWith("=" + Options.CombineDirectories.COMBINE_PER_INPUT_SUBDIR.getCombineMode()))
                     opt.combineDirs = Options.CombineDirectories.COMBINE_PER_INPUT_SUBDIR;
-                else if (str.endsWith("=" + Options.CombineDirectories.COMBINE_PER_INPUT_DIR.getCombineMode()))
+                else if (arg.endsWith("=" + Options.CombineDirectories.COMBINE_PER_INPUT_DIR.getCombineMode()))
                     opt.combineDirs = Options.CombineDirectories.COMBINE_PER_INPUT_DIR;
                 else
                     opt.combineDirs = Options.CombineDirectories.COMBINE_ALL_FILES;
             }
 
-            else if ("-in".equals(str)) {
+            else if ("-in".equals(arg)) {
                 next = App.OptionsNext.IN;
             }
-            else if ("-out".equals(str)) {
+            else if ("-out".equals(arg)) {
                 next = App.OptionsNext.OUT;
                 relPos = 2;
             }
-            else if ("-sql".equals(str)) {
+            else if ("-sql".equals(arg)) {
                 next = App.OptionsNext.SQL;
                 relPos = 3;
             }
-            else if ("-db".equals(str)) {
+            else if ("-db".equals(arg)) {
                 next = App.OptionsNext.DBPATH;
             }
 
-            else if ("-v".equals(str) || "--version".equals(str)) {
+            else if ("-v".equals(arg) || "--version".equals(arg)) {
                 String version = Utils.getVersion();
                 System.out.println(" CSV Cruncher version " + version);
                 System.exit(0);
+            }
+
+            else if (arg.startsWith("-")) {
+                System.out.println("ERROR: Unknown parameter: " + arg);
+                System.exit(1);
             }
 
             else {
                 if (next != null) {
                     switch (next) {
                         case IN:
-                            opt.inputPaths.add(str);
+                            opt.inputPaths.add(arg);
                             relPos = Math.max(relPos, 1);
                             continue;
                         case OUT:
-                            opt.outputPathCsv = str;
+                            opt.outputPathCsv = arg;
                             relPos = Math.max(relPos, 2);
                             continue;
                         case SQL:
-                            opt.sql = str;
+                            opt.sql = arg;
                             relPos = Math.max(relPos, 3);
                             continue;
                         case DBPATH:
-                            opt.dbPath = str;
+                            opt.dbPath = arg;
                             continue;
                         default:
                             next = null;
@@ -128,10 +134,10 @@ public class App
 
                 ++relPos;
                 if (relPos == 0) {
-                    opt.inputPaths.add(str);
+                    opt.inputPaths.add(arg);
                 }
                 else if (relPos == 1) {
-                    opt.outputPathCsv = str;
+                    opt.outputPathCsv = arg;
                 }
                 else {
                     if (relPos != 2) {
@@ -139,7 +145,7 @@ public class App
                         throw new IllegalArgumentException("Wrong arguments. Usage: crunch [-in] <inCSV> [-out] <outCSV> [-sql] <SQL> ...");
                     }
 
-                    opt.sql = str;
+                    opt.sql = arg;
                 }
             }
         }
