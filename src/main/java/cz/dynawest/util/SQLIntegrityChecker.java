196c5d56ae18bf2dc201d34bfdab8491dd454138
Ondrej Zizka <zizka@seznam.cz>
2018-10-08 09:35:45 +0200

    Refactor

--- src/main/java/cz/dynawest/util/SQLIntegrityChecker.java
+++ src/main/java/cz/dynawest/util/SQLIntegrityChecker.java
@@ -1,27 +1,21 @@
 package cz.dynawest.util;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Logger;
 
+/**
+ * Generates SQL to check integrity between two related SQL tables.
+ */
 public class SQLIntegrityChecker
 {
-    Logger log = Logger.getLogger(this.getClass().getName());
-
-    public void checkIntegrity(RelationInfo ri)
+    public void printCheckIntegritySql(RelationInfo ri)
     {
-        List asQueries = this.getCheckSql(ri);
-        Iterator iter = asQueries.iterator();
-
-        while (iter.hasNext()) {
-            String query = (String) iter.next();
+        List<String> asQueries = this.getCheckIntegritySqlCommands(ri);
+        for (String query: asQueries)
             System.out.println(query + ";");
-        }
-
     }
 
-    public List getCheckSql(RelationInfo ri)
+    public List<String> getCheckIntegritySqlCommands(RelationInfo ri)
     {
         String sSql = null;
         ArrayList asQueries = new ArrayList(2);
@@ -40,12 +34,18 @@ public class SQLIntegrityChecker
         }
 
         if (checkNonExistingParent) {
-            sSql = String.format("SELECT table2.`%4$s` AS has_no_parent, table2.`%5$s` AS referenced_parent \n FROM `%1$s` AS table1 RIGHT JOIN `%2$s` AS table2 ON table1.`%3$s` = table2.`%5$s` \n WHERE table2.`%5$s` IS NOT NULL AND table1.`%3$s` IS NULL", new Object[]{ri.parentTable, ri.childTable, ri.parentId, ri.childId, ri.childFK});
+            sSql = String.format("SELECT table2.`%4$s` AS has_no_parent, table2.`%5$s` AS referenced_parent" +
+                    " \n FROM `%1$s` AS table1 RIGHT JOIN `%2$s` AS table2 ON table1.`%3$s` = table2.`%5$s`" +
+                    " \n WHERE table2.`%5$s` IS NOT NULL AND table1.`%3$s` IS NULL",
+                    ri.parentTable, ri.childTable, ri.parentId, ri.childId, ri.childFK);
             asQueries.add(sSql);
         }
 
         if (checkNonExistingChild) {
-            sSql = String.format("SELECT table2.`%3$s` AS has_no_child, table2.`%4$s` AS referenced_child \n FROM `%1$s` AS table1 LEFT JOIN `%2$s` AS table2 ON table1.`%3$s` = table2.`%5$s` \n WHERE table2.`%4$s` IS NULL", new Object[]{ri.parentTable, ri.childTable, ri.parentId, ri.childId, ri.childFK});
+            sSql = String.format("SELECT table2.`%3$s` AS has_no_child, table2.`%4$s` AS referenced_child" +
+                    " \n FROM `%1$s` AS table1 LEFT JOIN `%2$s` AS table2 ON table1.`%3$s` = table2.`%5$s`" +
+                    " \n WHERE table2.`%4$s` IS NULL",
+                    ri.parentTable, ri.childTable, ri.parentId, ri.childId, ri.childFK);
             asQueries.add(sSql);
         }
 
