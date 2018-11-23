82f618172a00b509c1fd5cd2f4782c2ab9cbfe11
Ondrej Zizka <zizka@seznam.cz>
2018-11-23 18:04:03 +0100

    Improve test

--- src/test/java/cz/dynawest/csvcruncher/it/QueryPerInputSubpartIT.java
+++ src/test/java/cz/dynawest/csvcruncher/it/QueryPerInputSubpartIT.java
@@ -1,40 +1,47 @@
 package cz.dynawest.csvcruncher.it;
 
 import cz.dynawest.csvcruncher.CsvCruncherTestUtils;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * This is meant to reproduce a bug on existing environment, not to be run automatically.
+ * The testdata contain a change in columns structure, so CSV Cruncher needs to be run with --queryPerInputSubpart.
  */
 public class QueryPerInputSubpartIT
 {
-    String inPath = "src/test/data/sample-queryPerInputSubpart/oauth_consumer";
+    Path inPath = Paths.get("src/test/data/sample-queryPerInputSubpart/oauth_consumer");
 
 
     @Test
     public void queryPerInputSubpart() throws Exception
     {
+        Path outputDir = Paths.get("target/testResults/queryPerInputSubpart.csv");
+
         String command = "--json | --combineInputs | --queryPerInputSubpart | --rowNumbers" +
                 " | -in  | " + inPath +
                 // " | --exclude=.*/LOAD.*\\.csv" +
-                " | -out | target/testResults/queryPerInputSubpart.csv" +
+                " | -out | " + outputDir +
                 " | -sql | SELECT $table.* FROM $table";
 
         CsvCruncherTestUtils.runCruncherWithArguments(command);
-
-        // TODO: Add the verifications.
+        checkOutputFiles(outputDir);
+        // TODO: Add more verifications.
     }
 
     @Test
     public void queryPerInputSubpart_defaultSQL() throws Exception
     {
+        Path outputDir = Paths.get("target/testResults/queryPerInputSubpart_defaultSQL.csv");
+
         String command = "--json | --combineInputs | --queryPerInputSubpart | --rowNumbers" +
                 " | -in  | " + inPath +
                 // " | --exclude=.*/LOAD.*\\.csv" +
-                " | -out | target/testResults/queryPerInputSubpart_defaultSQL.csv";
+                " | -out | " + outputDir;
 
         CsvCruncherTestUtils.runCruncherWithArguments(command);
+        checkOutputFiles(outputDir);
 
         // TODO: Add the verifications.
     }
@@ -55,6 +62,13 @@ public class QueryPerInputSubpartIT
             Assert.assertTrue(ex.getMessage().contains("queryPerInputSubpart"));
             Assert.assertTrue(ex.getMessage().contains("$table"));
         }
+        Assert.fail("Should have thrown IllegalArgumentException, --queryPerInputSubpart needs generic SQL.");
     }
 
+    private void checkOutputFiles(Path outputDir)
+    {
+        Assert.assertTrue(outputDir.toFile().exists());
+        Assert.assertTrue(outputDir.resolve("oauth_consumer_1.csv.json").toFile().exists());
+        Assert.assertTrue(outputDir.resolve("oauth_consumer_2.csv.json").toFile().exists());
+    }
 }
