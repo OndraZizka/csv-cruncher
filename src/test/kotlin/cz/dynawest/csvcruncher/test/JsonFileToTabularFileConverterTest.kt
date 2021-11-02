232fde803b1b007e7bb335a4944250c8c06e7547
Ondrej Zizka <ondrej_zizka@swissre.com>
2021-11-02 18:49:42 +0100

    Extend tests for JSON tests

--- src/test/kotlin/cz/dynawest/csvcruncher/test/JsonFileToTabularFileConverterTest.kt
+++ src/test/kotlin/cz/dynawest/csvcruncher/test/JsonFileToTabularFileConverterTest.kt
@@ -14,9 +14,61 @@ import java.nio.file.Path
 class JsonFileToTabularFileConverterTest {
 
     @Test
-    fun testConvertJson() {
+    fun testConvertJson_01_arrayAtRoot_sameProperties() {
+        val entries = prepareEntriesFromFile("01-arrayAtRoot-sameProperties.json")
+
+        Assertions.assertThat(entries).size().isEqualTo(3)
+        Assertions.assertThat(entries).element(0).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "1"))
+        Assertions.assertThat(entries).element(0).extracting({it["name"]}).isEqualTo(MyProperty.StringMyProperty("name", "Ada"))
+        Assertions.assertThat(entries).element(1).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "2"))
+        Assertions.assertThat(entries).element(2).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "3"))
+    }
+
+    @Test
+    fun `testConvertJson_02-arrayAtRoot-propertiesMissing`() {
+        val entries = prepareEntriesFromFile("02-arrayAtRoot-propertiesMissing.json")
+
+        Assertions.assertThat(entries).size().isEqualTo(3)
+        Assertions.assertThat(entries).element(0).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "1"))
+        Assertions.assertThat(entries).element(0).extracting({it["name"]}).isNull()
+        Assertions.assertThat(entries).element(1).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "2"))
+        Assertions.assertThat(entries).element(1).extracting({it["name"]}).isEqualTo(MyProperty.StringMyProperty("name", "Ondra"))
+        Assertions.assertThat(entries).element(2).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "3"))
+        Assertions.assertThat(entries).element(2).extracting({it["born"]}).isEqualTo(MyProperty.StringMyProperty("born", "2016"))
+    }
+
+    @Test
+    fun `testConvertJson_03-arrayAtRoot-varyingProperties`() {
+        val entries = prepareEntriesFromFile("03-arrayAtRoot-varyingProperties.json")
+
+        Assertions.assertThat(entries).size().isEqualTo(3)
+        Assertions.assertThat(entries).element(0).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "1"))
+        Assertions.assertThat(entries).element(0).extracting({it["name"]}).isEqualTo(MyProperty.StringMyProperty("name", "Ada"))
+        Assertions.assertThat(entries).element(0).extracting({it["surname"]}).isNull()
+        Assertions.assertThat(entries).element(1).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "2"))
+        Assertions.assertThat(entries).element(1).extracting({it["surname"]}).isEqualTo(MyProperty.StringMyProperty("surname", "Zizka"))
+        Assertions.assertThat(entries).element(1).extracting({it["name"]}).isNull()
+        Assertions.assertThat(entries).element(2).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "3"))
+        Assertions.assertThat(entries).element(2).extracting({it["name"]}).isEqualTo(MyProperty.StringMyProperty("name", "Ondra"))
+        Assertions.assertThat(entries).element(2).extracting({it["surname"]}).isNull()
+    }
+
+    @Test
+    fun `testConvertJson_04-arrayAtRoot-differentTypes`() {
+        val entries = prepareEntriesFromFile("04-arrayAtRoot-differentTypes.json")
+
+        Assertions.assertThat(entries).size().isEqualTo(5)
+        Assertions.assertThat(entries).element(0).extracting({it["id"]}).isEqualTo(MyProperty.StringMyProperty("id", "1"))
+        Assertions.assertThat(entries).element(0).extracting({it["age"]}).isEqualTo(MyProperty.StringMyProperty("age", "4"))
+        Assertions.assertThat(entries).element(1).extracting({it["age"]}).isEqualTo(MyProperty.NumberMyProperty("age", 15))
+        Assertions.assertThat(entries).element(2).extracting({it["age"]}).isEqualTo(MyProperty.BooleanMyProperty("age", true))
+        Assertions.assertThat(entries).element(3).extracting({it["age"]}).isEqualTo(MyProperty.NullMyProperty("age"))
+        Assertions.assertThat(entries).element(4).extracting({it["age"]}).isNull()
+    }
+
+    private fun prepareEntriesFromFile(testFilePath: String): MutableList<Map<String, MyProperty>> {
         val converter = JsonFileToTabularFileConverter()
-        val inputStream: InputStream = ResourceLoader.openResourceAtRelativePath(Path.of("01-arrayAtRoot-sameProperties.json"))
+        val inputStream: InputStream = ResourceLoader.openResourceAtRelativePath(Path.of(testFilePath))
 
         val entries = mutableListOf<Map<String, MyProperty>>()
         converter.processEntries(inputStream, Path.of("/"), object : EntryProcessor {
@@ -26,8 +78,7 @@ class JsonFileToTabularFileConverterTest {
                 log.info("Entry: ${entryMap}")
             }
         })
-
-        Assertions.assertThat(entries).size().isEqualTo(3)
+        return entries
     }
 
     companion object { private val log = logger() }
