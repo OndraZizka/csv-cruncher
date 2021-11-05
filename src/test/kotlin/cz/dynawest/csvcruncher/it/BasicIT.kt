f0cb37b0aac5b4e973f28327402c4476db4e0f6d
Ondrej Zizka <ondrej_zizka@swissre.com>
2021-11-05 15:20:41 +0100

    Add real sample JSON data from https://github.com/jdorfman/awesome-json-datasets#nasa

--- src/test/kotlin/cz/dynawest/csvcruncher/it/BasicIT.kt
+++ src/test/kotlin/cz/dynawest/csvcruncher/it/BasicIT.kt
@@ -10,23 +10,9 @@ import java.util.*
 
 class BasicIT {
 
-    /*
-    * T--json=entries
-03:38:47.383 [main] DEBUG cz.dynawest.csvcruncher.app.OptionsParser -  * --rowNumbers
-03:38:47.383 [main] DEBUG cz.dynawest.csvcruncher.app.OptionsParser -  * -in
-03:38:47.384 [main] DEBUG cz.dynawest.csvcruncher.app.OptionsParser -  * src/test/data/eapBuilds.csv
-03:38:47.384 [main] DEBUG cz.dynawest.csvcruncher.app.OptionsParser -  * -out
-03:38:47.384 [main] DEBUG cz.dynawest.csvcruncher.app.OptionsParser -  * target/results/result.csv
-03:38:47.384 [main] DEBUG cz.dynawest.csvcruncher.app.OptionsParser -  * -sql
-03:38:47.384 [main] DEBUG cz.dynawest.csvcruncher.app.OptionsParser -  * SELECT jobName, buildNumber, config, ar, arFile, deployDur, warmupDur, scale,
-                                CAST(warmupDur AS DOUBLE) / CAST(deployDur AS DOUBLE) AS warmupSlower
-                                FROM eapBuilds ORDER BY deployDur
-     */
-
-    var inPath = Paths.get("src/test/data/eapBuilds.csv")
-
     @Test
     fun queryPerInputSubpart() {
+        var inPath = Paths.get("src/test/data/eapBuilds.csv")
         val outputDir = Paths.get("target/results/queryPerInputSubpart.csv")
         val command =
             """--json=entries | --rowNumbers | -in  | $inPath | -out | $outputDir | -sql |
