923eb99f789554b3e4788d7b47364cf34c8d8475
Ondrej Zizka <ondrej_zizka@swissre.com>
2021-11-07 13:00:03 +0100

    Extend and fix the test of: #17 Quote all SQL identifiers to retain the case.

--- src/test/kotlin/cz/dynawest/csvcruncher/app/OptionsParsingTest.kt
+++ src/test/kotlin/cz/dynawest/csvcruncher/app/OptionsParsingTest.kt
@@ -1,6 +1,8 @@
 package cz.dynawest.csvcruncher.app
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.nio.file.Path
 
 class OptionsParsingTest {
 
@@ -10,11 +12,30 @@ class OptionsParsingTest {
             " |  --exclude=.*/LOAD.*\\.csv" +
             " |  -in  | src/test/data/sample-collab/apollo_recording_group/" +
             " |  -out | target/testResults/apollo_recording_group.csv" +
-            " |  -sql | SELECT * FROM apollo_recording_group"
+            " |  -sql | SELECT * FROM apollo_recording_group" +
+            " |  -initSql | foo/init1.sql | foo/init2.sql"
 
         val arguments = command.split("|").map { it.trim() }
-        OptionsParser.parseArgs(arguments.toTypedArray())
+        val options = OptionsParser.parseArgs(arguments.toTypedArray())!!
 
+        // The default values
+        Assertions.assertThat(options.jsonExportFormat).isEqualTo(Options.JsonExportFormat.ENTRY_PER_LINE)
+        Assertions.assertThat(options.combineInputFiles).isEqualTo(Options.CombineInputFiles.CONCAT)
+
+        // Defaults for imports/exports
+        Assertions.assertThat(options.excludePathsRegex.toString()).isEqualTo(".*/LOAD.*\\.csv")
+
+        // Per import/export
+        Assertions.assertThat(options.importArguments).size().isEqualTo(1)
+        Assertions.assertThat(options.importArguments).element(0).extracting{it.path}.isEqualTo(Path.of("src/test/data/sample-collab/apollo_recording_group/"))
+
+        Assertions.assertThat(options.exportArguments).size().isEqualTo(1)
+        Assertions.assertThat(options.exportArguments).element(0).extracting{it.path}.isEqualTo(Path.of("target/testResults/apollo_recording_group.csv"))
+        Assertions.assertThat(options.exportArguments).element(0).extracting{it.sqlQuery}.isEqualTo("SELECT * FROM apollo_recording_group")
+
+        Assertions.assertThat(options.initSqlArguments).size().isEqualTo(2)
+        Assertions.assertThat(options.initSqlArguments).element(0).extracting{it.path}.isEqualTo(Path.of("foo/init1.sql"))
+        Assertions.assertThat(options.initSqlArguments).element(1).extracting{it.path}.isEqualTo(Path.of("foo/init2.sql"))
     }
 
 }
