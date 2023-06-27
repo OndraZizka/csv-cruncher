f07b14905a710753cf8b74bb1348ac06de6f6ee2
Ondrej Zizka <ondrej.zizka@nexxiot.com>
2023-06-27 11:17:50 +0200

    Code cleanup

--- src/test/kotlin/cz/dynawest/csvcruncher/it/ChangedSchemaIT.kt
+++ src/test/kotlin/cz/dynawest/csvcruncher/it/ChangedSchemaIT.kt
@@ -4,14 +4,10 @@ import cz.dynawest.csvcruncher.Cruncher
 import cz.dynawest.csvcruncher.CsvCruncherTestUtils
 import cz.dynawest.csvcruncher.Options2
 import cz.dynawest.csvcruncher.app.OptionsEnums
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.*
 
 class ChangedSchemaIT {
-    @BeforeEach
-    fun dropDbSchema() {
-    }
 
     @Test
     @Throws(Exception::class)
@@ -30,11 +26,6 @@ class ChangedSchemaIT {
         options.queryPerInputSubpart = true // This is key.
         options.initialRowNumber = 1L
 
-        // TODO: This test fails, because the tables created are NULL_1, NULL_2
-        //      which is OK per se, but I need to implement dumping without -sql
-        //      and dump all automaticaly.
-
-        // TODO: Also let's switch to LogBack - JUL sucks.
         Cruncher(options).crunch()
     }
 }
